36e9c14613b1ff7c2ef8a7500967958c80c99d0f
lht <2553265118@qq.com>
2024-11-08 13:19:43 +0800

    init

---
+++ component/calendar.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<uni-calendar 
+		ref="calendar"
+		:insert="false"
+		@confirm="confirm"
+		 />
+		 <button  class="calendar-button" @click="open">打开日历</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		methods: {
+			open(){
+				this.$refs.calendar.open();
+			},
+			confirm(e) {
+				console.log(e);
+			}
+		}
+	}
+</script>
+
+<style>
+	.calendar-button{
+		margin-top: 10px;
+		color: white;
+		background-color: black;
+		width:100px;
+	}
+</style>
