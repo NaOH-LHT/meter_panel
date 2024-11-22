2a100800b06febb3bbcabdb9822bb969933cf3c7
tjj <13430369+TtAaNnGgJjUuNnJjIiEe@user.noreply.gitee.com>
2024-11-22 17:16:10 +0800

    Wifi

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,10 @@
 <template>
 	<view>
 		<uni-card>
-			<button @click="connect">连接串口</button>
+			<button @click="start">打开 Wi-Fi 模块</button>
+			<button @click="stop">关闭 Wi-Fi 模块</button>
+			<button @click="getDetail">获取已连接的 Wi-Fi 信息</button>
+			<button @click="getList">获取 Wi-Fi 列表</button>
 			<view class="" v-if="isConnect">
 				<button @click="goDisplay">
 					数据展示
@@ -14,25 +17,85 @@
 	</view>
 </template>
 <script>
-	
 	export default {
-		data(){
-			return{
-				isConnect:false
+		data() {
+			return {
+				isConnect: false
 			}
 		},
-		methods:{
-			connect(){
-				this.isConnect=true
-			},
-			goDisplay(){
-				uni.navigateTo({
-					url:'/pages/display/display'
+		onLoad(){
+			//获取到 Wi-Fi 列表数据事件的监听函数
+			uni.onGetWifiList((e)=>{
+				console.log('onGetWifiList',e)
+			})
+		},
+		onUnload(){
+			//移除获取到 Wi-Fi 列表数据事件的监听函数。
+			uni.offGetWifiList()
+		},
+		methods: {
+			start() { // 连接
+				this.isConnect = true //连接成功后调用
+				uni.startWifi({
+					success(e) {
+						console.log('startWifiSuccess', e)
+					},
+					fail(e) {
+						console.log('startWifiFail', e)
+					},
+					complete(e) {
+						console.log('startWifiComplete', e)
+					}
 				})
 			},
-			goInput(){
+			stop(){
+				uni.stopWifi({
+					success(e) {
+						console.log('stopWifiSuccess', e)
+					},
+					fail(e) {
+						console.log('stopWifiFail', e)
+					},
+					complete(e) {
+						console.log('stopWifiComplete', e)
+					}
+				})
+			},
+			getDetail(){
+				uni.getConnectedWifi({
+					partialInfo:false,
+					success(e) {
+						console.log('getConnectedWifiSuccess', e)
+					},
+					fail(e) {
+						console.log('getConnectedWifiFail', e)
+					},
+					complete(e) {
+						console.log('getConnectedWifiComplete', e)
+					}
+				})
+			},
+			getList(){
+				uni.getWifiList({
+					success(e) {
+						console.log("getWifiList_success",e)
+					},
+					fail(e) {
+								console.log("getWifiList_fail",e)
+					},
+					complete(e) {
+						console.log("getWifiList_complete",e)
+					}
+				})
+			},
+			goDisplay() {
 				uni.navigateTo({
-					url:'/pages/input/input'
+					url: '/pages/display/display'
+				})
+			},
+			goInput() {
+				uni.navigateTo({
+					url: '/pages/input/input'
 				})
 			}
 		}
